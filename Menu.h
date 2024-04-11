94ff4ccb775b8966904ce4cd284f5389820d335f
Yanelly Mego <89563053+yanellymego@users.noreply.github.com>
2024-04-11 01:16:06 -0700

    Updated to satisfy linter

--- Menu.h
+++ Menu.h
@@ -1,25 +1,41 @@
-#ifndef MENU_H
-#define MENU_H
+// NOLINT(legal/copyright)
+// NOLINT(build/namespaces)
 
+
+#ifndef MENU_H_
+#define MENU_H_
+
+#include "Birthday.h"
 #include <iostream>
+#include <limits>
 #include <list>
 #include <sstream>
 #include <string>
-#include <limits>
-#include "Birthday.h"
-using namespace std;
+
+// Using-declarations 
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::istringstream;
+using std::list;
+using std::max;
+using std::numeric_limits;
+using std::streamsize;
+using std::stringstream;
 
 class Menu {
-    public:
-        Menu(const string &defaultInput = "");
-        ~Menu();
-        void Option1();
-        void Option2();
-        void Option3();
-    private:
-        list<Birthday*> all_birthdays;
-        list<string> test_string; // for unit testing
-        string data;
+  public:
+    explicit Menu(const string &defaultInput = "");
+    ~Menu();
+    void Option1();
+    void Option2();
+    void Option3();
+
+  private:
+    list<Birthday *> all_birthdays;
+    list<string> test_string;  // for unit testing
+    string data;
 };
 
-#endif
+#endif  // MENU_H_
